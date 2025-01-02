c7f0054fd20b145eb31af4229132ea013089234d
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-01-02 11:58:03 +0530

    Create Two Sum II Input Array Is Sorted.cpp

---
+++ Neetcode 150/Two Pointers/Two Sum II Input Array Is Sorted.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n=numbers.size();
+        int i=0,j=n-1;
+        vector<int>ans={-1,-1};
+        while(i<j){
+            if(numbers[i]+numbers[j]==target){
+                ans[0]=i+1;
+                ans[1]=j+1;
+                break;
+            }
+            else if(numbers[i]+numbers[j]<target){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return ans;
+    }
+};
