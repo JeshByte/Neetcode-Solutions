f6dff8c805366ba4d98a939619e7133dee91b69e
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-01-23 13:09:04 +0530

    Update Valid Parentheses.cpp

--- Neetcode 150/Stack/Valid Parentheses.cpp
+++ Neetcode 150/Stack/Valid Parentheses.cpp
@@ -1,5 +1,38 @@
-#include<iostream.h>
-int main(){
-  std::cout<<"Hello World\n";
-  return 0;
-}
+class Solution {
+public:
+    char findComp(char c){
+        char comp;
+        if(c==')'){
+            comp='(';
+        }
+        else if(c==']'){
+            comp='[';
+        }
+        else{
+            comp='{';
+        }
+        return comp;
+    }
+    bool isValid(string s) {
+        stack<char>st;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else{
+                char comp=findComp(s[i]);
+                if(!st.empty() && st.top()==comp){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(!st.empty()){
+            return false;
+        }
+        return true;
+    }
+};
