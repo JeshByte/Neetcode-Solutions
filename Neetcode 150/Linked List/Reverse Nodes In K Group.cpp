c3094c4699f99c4605afe526eece59d68888a25e
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-01-25 19:02:59 +0530

    Update Reverse Nodes In K Group.cpp

--- Neetcode 150/Linked List/Reverse Nodes In K Group.cpp
+++ Neetcode 150/Linked List/Reverse Nodes In K Group.cpp
@@ -1,3 +1,44 @@
+// Recursive Solution
+// class Solution {
+// public:
+//     ListNode* reversed(ListNode* head){
+//         ListNode* curr=head;
+//         ListNode* prev=NULL;
+//         while(head!=NULL){
+//             head=head->next;
+//             curr->next=prev;
+//             prev=curr;
+//             curr=head;
+//         }
+//         return prev;
+//     }
+//     ListNode* reverseKGroup(ListNode* head, int k) {
+//         if(head==NULL){
+//             return head;
+//         }
+//         ListNode* after=head;
+//         int i=1;
+//         while(after!=NULL && i<=k-1){
+//             after=after->next;
+//             i++;
+//         }
+//         if(after==NULL){
+//             return head;
+//         }
+//         ListNode* temp;
+//         temp=reverseKGroup(after->next,k);
+//         after->next=NULL;
+//         head=reversed(head);
+//         ListNode* last=head;
+//         while(last->next!=NULL){
+//             last=last->next;
+//         }
+//         last->next=temp;
+//         return head;
+//     }
+// };
+
+// Iterative Solution(O(1) Space)
 class Solution {
 public:
     ListNode* reversed(ListNode* head){
