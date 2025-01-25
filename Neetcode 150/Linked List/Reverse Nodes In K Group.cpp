41414a58bf82d7a0d25c09933069c9a3369e7f41
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-01-25 19:00:28 +0530

    Create Reverse Nodes In K Group.cpp

---
+++ Neetcode 150/Linked List/Reverse Nodes In K Group.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    ListNode* reversed(ListNode* head){
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        while(head!=NULL){
+            head=head->next;
+            curr->next=prev;
+            prev=curr;
+            curr=head;
+        }
+        return prev;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* dummy=new ListNode(0);
+        ListNode* dummy_tail=dummy;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            ListNode* store=temp;
+            for(int i=1;i<=k-1;i++){
+                if(temp){
+                temp=temp->next;
+                }
+                else{
+                    break;
+                }
+            }
+            if(temp==NULL){
+                dummy_tail->next=store;
+                dummy_tail=dummy_tail->next;
+                break;
+            }
+            ListNode* tp=temp->next;
+            temp->next=NULL;
+            temp=head;
+            head=reversed(head);
+            dummy_tail->next=head;
+            dummy_tail=temp;
+            temp=tp;
+            head=tp;
+        }
+        ListNode* newhead=dummy->next;
+        dummy->next=NULL;
+        delete dummy;
+        return newhead;
+    }
+};
